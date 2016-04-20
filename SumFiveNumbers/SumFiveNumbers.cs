97ff44f5cf64de18c12cc62097e99767236716c3
DobrinStefkin <dobrin.stefkin@abv.bg>
2016-04-20 21:42:48 +0300

    My homework on Console In and Out; course: C# part 1

--- SumFiveNumbers/SumFiveNumbers.cs
+++ SumFiveNumbers/SumFiveNumbers.cs
@@ -5,46 +5,46 @@ class SumThreeNumbers
     static void Main()
     {
         int a = int.Parse(Console.ReadLine());
-        if (-1000 > a || a > 1000)
-        {
-            Console.WriteLine("Invalid input!");
-        }
-        else
-        {
+        //if (-1000 > a || a > 1000)
+        //{
+        //    Console.WriteLine("Invalid input!");
+        //}
+        //else
+        //{
             int b = int.Parse(Console.ReadLine());
-            if (-1000 > b || b > 1000)
-            {
-                Console.WriteLine("Invalid input!");
-            }
-            else
-            {
+            //if (-1000 > b || b > 1000)
+            //{
+            //    Console.WriteLine("Invalid input!");
+            //}
+            //else
+            //{
                 int c = int.Parse(Console.ReadLine());
-                if (-1000 > c || c > 1000)
-                {
-                    Console.WriteLine("Invalid input!");
-                }
-                else
-                {
+                //if (-1000 > c || c > 1000)
+                //{
+                //    Console.WriteLine("Invalid input!");
+                //}
+                //else
+                //{
                     int d = int.Parse(Console.ReadLine());
-                    if (-1000 > d || d > 1000)
-                    {
-                        Console.WriteLine("Invalid input!");
-                    }
-                    else
-                    {
+                    //if (-1000 > d || d > 1000)
+                    //{
+                    //    Console.WriteLine("Invalid input!");
+                    //}
+                    //else
+                    //{
                         int e = int.Parse(Console.ReadLine());
-                        if (-1000 > e || e > 1000)
-                        {
-                            Console.WriteLine("Invalid input!");
-                        }
-                        else
-                        {
+                        //if (-1000 > e || e > 1000)
+                        //{
+                        //    Console.WriteLine("Invalid input!");
+                        //}
+                        //else
+                        //{
                             int sum = (a + b + c + d + e);
                             Console.WriteLine(sum);
-                        }
-                    }
-                }
-            }
-        }
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
